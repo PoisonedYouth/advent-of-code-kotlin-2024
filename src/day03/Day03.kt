5ab3cc188f755fce35cc09c7926454591cb1f94c
Matthias Schenk <matthias.schenk@dwins.de>
2024-12-03 08:03:21 +0100

    Day03

---
+++ src/day03/Day03.kt
@@ -0,0 +1,52 @@
+package day03
+
+import println
+import readInput
+
+fun main() {
+    val regex1 = "mul\\([0-9]{1,3},[0-9]{1,3}\\)".toRegex()
+    val regex2 = "(mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don't\\(\\))".toRegex()
+
+    fun MatchResult.extractNumbers(): Pair<Int, Int> = this.value.substringAfter("mul(").substringBefore(")").split(",").map { it.toInt() }.let {
+        it[0] to it[1]
+    }
+
+    fun Pair<Int, Int>.multiply(): Int = this.first * this.second
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf { line ->
+            regex1.findAll(line).sumOf { it.extractNumbers().multiply() }
+        }
+    }
+
+
+    fun part2(input: List<String>): Int {
+        var isEnabled = true
+        return input.sumOf { line ->
+            regex2.findAll(line).mapNotNull {
+                when {
+                    it.value == "don't()" -> {
+                        isEnabled = false
+                        null
+                    }
+
+                    it.value == "do()" -> {
+                        isEnabled = true
+                        null
+                    }
+
+                    it.value.contains("mul") && isEnabled -> {
+                        it.extractNumbers().multiply()
+                    }
+
+                    else -> null
+                }
+            }.sum()
+
+        }
+    }
+
+    val input = readInput("day03/Day03")
+    part1(input).println()
+    part2(input).println()
+}
